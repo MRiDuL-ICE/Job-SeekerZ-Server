e6146b5154688a0dd59f808612b32b6193fdd831
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-12 12:19:13 +0600

    project setup mongodb connection and job data fetch

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+const cors = require('cors');
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@phmridul.el21o.mongodb.net/?retryWrites=true&w=majority&appName=PHMRiDuL`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jobCollection = client.db('JobSeekerZDB').collection('jobs')
+
+
+    app.get('/jobs', async(req, res) => {
+        const result = await jobCollection.find().toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', async(req, res) => {
+    res.send('Job SeekerZ Server')
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`)
+})
